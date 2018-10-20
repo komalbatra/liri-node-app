80b6dee68b0f165d929feb277fb60b623151dbab
Komal Batra <KomalBatra@gmail.com>
2018-10-20 14:13:58 -0400

    NPMs installed

--- keys.js
+++ keys.js
@@ -5,3 +5,12 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+
+OMBD:
+
+Here is your key: b3c0b435
+
+Please append it to all of your API requests,
+
+OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=b3c0b435
